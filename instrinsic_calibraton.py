0c06f47491dab1fb1a90bc59f378ba3d4bbfbeb9
Ellen <ellenw2008@gmail.com>
2025-01-20 16:08:28 -0500

    calibration code

--- instrinsic_calibraton.py
+++ instrinsic_calibraton.py
@@ -1,16 +1,18 @@
 import numpy as np
 import cv2 as cv
 
+
+
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-objp = np.zeros((3*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:3,0:7].T.reshape(-1,2)
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:6,0:9].T.reshape(-1,2)
 
 objpoints = []
 imgpoints = []
 
-image = 'NORMAL00042.jpg'
+image = 'checker1.jpg'
 
 #for fname in image:
 
@@ -22,13 +24,13 @@ img_gray = cv.resize(gray, (100,200))
 # cv.waitKey(0)
 # Find the chess board corners
 
-test_image = cv.imread('img.png')
-gray = cv.cvtColor(test_image, cv.COLOR_BGR2GRAY)
+# test_image = cv.imread('img.png')
+# gray = cv.cvtColor(test_image, cv.COLOR_BGR2GRAY)
+# ret, corners = cv.findChessboardCorners(gray, (6, 9), None)
+# print("Test Chessboard detection result:", ret)
+
+
 ret, corners = cv.findChessboardCorners(gray, (6, 9), None)
-print("Test Chessboard detection result:", ret)
-
-
-ret, corners = cv.findChessboardCorners(gray, (3, 7), None)
 
 
 # If found, add object points, image points (after refining them)
@@ -40,7 +42,7 @@ if ret == True:
     imgpoints.append(corners2)
 
     # Draw and display the corners
-    cv.drawChessboardCorners(img, (3, 7), corners2, ret)
+    cv.drawChessboardCorners(img, (6, 9), corners2, ret)
     cv.imshow('img', img)
     print("i have reached here")
     cv.waitKey(0)
